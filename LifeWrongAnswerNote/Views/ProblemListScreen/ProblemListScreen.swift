d2522111af9310b40757613fecfd3cf640020413
Nam Geon Kim <namgeon1106@naver.com>
2022-04-30 17:43:38 +0900

    feat: 필터 뷰 구현

--- LifeWrongAnswerNote/Views/ProblemListScreen/ProblemListScreen.swift
+++ LifeWrongAnswerNote/Views/ProblemListScreen/ProblemListScreen.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ProblemListScreen: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 20) {
+                HStack(spacing: 16) {
+                    Menu {
+                        Button("sss", action: {})
+                        Button("ddd", action: {})
+                    } label: {
+                        CustomMenuLabel(title: "카테고리")
+                    }
+                    
+                    Menu {
+                        Button("sss", action: {})
+                        Button("ddd", action: {})
+                    } label: {
+                        CustomMenuLabel(title: "진행상태")
+                    }
+                    
+                    Spacer()
+                }
+                
+                CustomSearchBar(searchText: $searchText, placeholder: "제목으로 검색")
+                
+                Spacer()
+            }
+            .navigationTitle("고민들 목록")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.horizontal, 16)
+            .padding(.top, 20)
+        }
     }
 }
 
---
+++ LifeWrongAnswerNote/Views/ProblemListScreen/SubViews/CustomMenuLabel.swift
@@ -0,0 +1,36 @@
+//
+//  CustomMenuLabel.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/04/30.
+//
+
+import SwiftUI
+
+struct CustomMenuLabel: View {
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.subheadline)
+            Spacer()
+            Image(systemName: "chevron.down")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 10, height: 10)
+                .foregroundColor(Color(UIColor.systemGray3))
+            
+        }
+        .padding(.horizontal, 13)
+        .frame(width: 120, height: 32)
+        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray3), lineWidth: 1))
+        .tint(.black)
+    }
+}
+
+struct CustomMenuLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomMenuLabel(title: "카테고리")
+    }
+}
