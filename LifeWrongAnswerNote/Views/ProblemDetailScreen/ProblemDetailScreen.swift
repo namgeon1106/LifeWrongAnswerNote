4b587527df075a280464d70349bad7d92c4df347
Nam Geon Kim <namgeon1106@naver.com>
2022-04-30 23:40:14 +0900

    feat: 문제 읽기/수정 화면 전체 틀 구현

---
+++ LifeWrongAnswerNote/Views/ProblemDetailScreen/ProblemDetailScreen.swift
@@ -0,0 +1,58 @@
+//
+//  ProblemDetailScreen.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ProblemDetailScreen: View {
+    @State private var titleInput = ""
+    
+    var body: some View {
+        VStack {
+            InputTemplate(title: "제목       ") {
+                TextField("제목을 입력하세요", text: .constant(""))
+            }
+            .padding(.bottom, 9)
+            
+            InputTemplate(title: "카테고리") {
+                HStack {
+                    Menu {
+                        Button("A", action: {})
+                        Button("B", action: {})
+                    } label: {
+                        CustomMenuLabel(title: "카테고리")
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.bottom, 38)
+            
+            TabView {
+                Text("1")
+                Text("2")
+            }.tabViewStyle(PageTabViewStyle())
+            
+            RoundedRectangle(cornerRadius: 2)
+                .frame(height: 4)
+                .foregroundColor(Color(UIColor.systemGray4))
+            
+            Spacer()
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 23)
+        .padding(.bottom, 16)
+        .navigationBarTitle("문제 읽기/수정")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ProblemDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ProblemDetailScreen()
+        }
+    }
+}
---
+++ LifeWrongAnswerNote/Views/ProblemDetailScreen/SubViews/InputTemplate.swift
@@ -0,0 +1,42 @@
+//
+//  InputTemplate.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/04/30.
+//
+
+import SwiftUI
+
+struct InputTemplate<Content: View>: View {
+    var title: String
+    var content: () -> Content
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 43) {
+                Text(title)
+                content()
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 11)
+            Divider()
+        }
+    }
+}
+
+struct InputTemplate_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 9) {
+            InputTemplate(title: "제목       ") {
+                TextField("제목을 입력하세요", text: .constant(""))
+            }
+            
+            InputTemplate(title: "카테고리") {
+                HStack {
+                    CustomMenuLabel(title: "카테고리")
+                    Spacer()
+                }
+            }
+        }
+    }
+}
