3b286f4287edfd116899c8e02b9f5ca154e3e657
Nam Geon Kim <namgeon1106@naver.com>
2022-05-01 15:21:25 +0900

    feat: 각 선택지 view 구현

---
+++ LifeWrongAnswerNote/Views/ProblemDetailScreen/SubViews/Choice.swift
@@ -0,0 +1,42 @@
+//
+//  Choice.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct Choice: View {
+    let selected: Bool
+    let title: String
+    @State private var titleInput: String
+    var color: Color {
+        selected ? .green : .gray
+    }
+    
+    init(selected: Bool, title: String) {
+        self.selected = selected
+        self.title = title
+        self.titleInput = title
+    }
+    
+    var body: some View {
+        HStack(spacing: 13) {
+            Image(systemName: selected ? "checkmark.circle.fill" : "circle")
+            TextField("", text: $titleInput)
+                .disabled(true)
+            Spacer()
+        }
+        .padding(.vertical, 14)
+        .padding(.horizontal, 11)
+        .foregroundColor(color)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(color))
+    }
+}
+
+struct Choice_Previews: PreviewProvider {
+    static var previews: some View {
+        Choice(selected: true, title: "선택 1")
+    }
+}
