631ddba84e659ede381c32c8a008533fb85ddb2f
Nam Geon Kim <namgeon1106@naver.com>
2022-05-01 16:31:53 +0900

    feat: 문제 읽기/수정 화면 구현

--- LifeWrongAnswerNote/Views/ProblemDetailScreen/ProblemDetailScreen.swift
+++ LifeWrongAnswerNote/Views/ProblemDetailScreen/ProblemDetailScreen.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ProblemDetailScreen: View {
     @State private var titleInput = ""
+    @State private var situationInput = ""
+    @State private var reasonInput = ""
+    @State private var resultDetailInput = ""
+    @State private var retrospectInput = ""
+    
+    @State private var choices = ["선택 1", "선택 2"]
+    @State private var chosen: Int? = nil
     
     var body: some View {
         VStack {
@@ -28,11 +35,52 @@ struct ProblemDetailScreen: View {
                     Spacer()
                 }
             }
+            .padding(.bottom, 9)
+            
+            InputTemplate(title: "평가       ") {
+                HStack {
+                    Menu {
+                        Button("A", action: {})
+                        Button("B", action: {})
+                    } label: {
+                        CustomMenuLabel(title: "평가")
+                    }
+                    Spacer()
+                }
+            }
             .padding(.bottom, 20)
             
             TabView {
-                Text("1")
-                Text("2")
+                InputDetailTemplate(title: "1. 무슨 상황인가요?") {
+                    CustomTextEditor(text: $situationInput)
+                }
+                
+                InputDetailTemplate(title: "2. 가능한 선택과 내가 한 선택은?") {
+                    VStack {
+                        ForEach(0..<choices.count) { index in
+                            Choice(selected: index == chosen, title: choices[index])
+                                .onTapGesture {
+                                    chosen = index
+                                }
+                        }
+                        Button("+ 선택 추가") {
+                            choices.append("새로운 선택")
+                        }
+                        .padding(.top, 10)
+                    }
+                }
+                
+                InputDetailTemplate(title: "3. 선택의 이유는?") {
+                    CustomTextEditor(text: $reasonInput)
+                }
+                
+                InputDetailTemplate(title: "4. 선택의 결과는?") {
+                    CustomTextEditor(text: $reasonInput)
+                }
+                
+                InputDetailTemplate(title: "5. 느낀점, 회고") {
+                    CustomTextEditor(text: $retrospectInput)
+                }
             }
             .tabViewStyle(PageTabViewStyle())
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
