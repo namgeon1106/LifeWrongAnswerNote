0aa9fa919a629fbffcf6c2bef2bd6dd1e2e62aaa
Nam Geon Kim <namgeon1106@naver.com>
2022-05-13 11:10:23 +0900

    feat: category list view model 구현

--- LifeWrongAnswerNote/ViewModels/CategoryListViewModel.swift
+++ LifeWrongAnswerNote/ViewModels/CategoryListViewModel.swift
@@ -8,6 +8,34 @@
 import Foundation
 import CoreData
 
+class CategoryListViewModel: ObservableObject {
+    @Published var categoryVMs = [CategoryViewModel]()
+    
+    func showAllCategories() {
+        self.categoryVMs = Category.all().map(CategoryViewModel.init(category:))
+    }
+    
+    func showFilteredCategories(searchText: String) {
+        self.categoryVMs = Category.filteredBy(searchText).map(CategoryViewModel.init(category:))
+    }
+    
+    func addCategory(name: String) {
+        let category = Category(context: Category.viewContext)
+        category.name = name
+        category.problems = []
+        CoreDataManager.shared.save()
+    }
+    
+    func renameCategory(categoryVM: CategoryViewModel, newName: String) {
+        categoryVM.category.rename(newName: newName)
+    }
+    
+    func deleteCategory(categoryVM: CategoryViewModel) {
+        categoryVM.category.delete()
+        CoreDataManager.shared.save()
+    }
+}
+
 struct CategoryViewModel {
     let category: Category
     
