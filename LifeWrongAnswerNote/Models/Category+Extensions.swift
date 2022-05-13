68ba0088751a0f3977e4c8535505ce5a4d6a3d08
Nam Geon Kim <namgeon1106@naver.com>
2022-05-13 10:28:27 +0900

    feat: category base model 구현

---
+++ LifeWrongAnswerNote/Models/Category+Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Category+Extensions.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/05/13.
+//
+
+import Foundation
+import CoreData
+
+extension Category: BaseModel {
+    static func byName(_ name: String) -> Category? {
+        let request = Category.fetchRequest()
+        request.predicate = NSPredicate(format: "%K = %@", #keyPath(Category.name), name)
+        
+        do {
+            let categoryArr = try Self.viewContext.fetch(request)
+            return categoryArr.isEmpty ? nil : categoryArr[0]
+        } catch {
+            return nil
+        }
+    }
+    
+    static func filteredBy(_ subString: String) -> [Category] {
+        if subString.isEmpty {
+            return Category.all()
+        }
+        
+        let request = Category.fetchRequest()
+        request.predicate = NSPredicate(format: "INSTR(%K, %@) > 0", #keyPath(Category.name), subString)
+        
+        do {
+            return try Self.viewContext.fetch(request)
+        } catch {
+            return []
+        }
+    }
+}
