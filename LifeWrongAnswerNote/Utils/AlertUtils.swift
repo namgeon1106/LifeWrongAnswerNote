6fabf8d80a26caa0d39e65bebf80875dbd866dac
Nam Geon Kim <namgeon1106@naver.com>
2022-05-13 12:21:57 +0900

    refactor: alert utils 클래스 리펙토링 및 추가적인 alert 메서드 추가

--- LifeWrongAnswerNote/Utils/AlertUtils.swift
+++ LifeWrongAnswerNote/Utils/AlertUtils.swift
@@ -11,6 +11,13 @@ import UIKit
 class AlertUtils {
     static var alertTextInput = ""
     
+    static func displayAlert(with alert: UIAlertController) {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScenes = scenes.first as? UIWindowScene
+            
+        windowScenes?.windows.first?.rootViewController?.present(alert, animated: true)
+    }
+    
     static func displayAlertViewWithTextField(title: String, message: String, placeholder: String, okMessage: String, okStyle: UIAlertAction.Style, okAction: @escaping () -> Void) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
@@ -18,7 +25,7 @@ class AlertUtils {
             name.placeholder = placeholder
         }
             
-        let addCategoryAction = UIAlertAction(title: okMessage, style: okStyle) { _ in
+        let okAlertAction = UIAlertAction(title: okMessage, style: okStyle) { _ in
             alertTextInput = (alert.textFields?[0].text)!
             okAction()
         }
@@ -27,19 +34,16 @@ class AlertUtils {
             print("Cancel clicked")
         }
             
-        alert.addAction(addCategoryAction)
+        alert.addAction(okAlertAction)
         alert.addAction(cancelAction)
             
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScenes = scenes.first as? UIWindowScene
-            
-        windowScenes?.windows.first?.rootViewController?.present(alert, animated: true)
+        displayAlert(with: alert)
     }
     
     static func displayAlertView(title: String, message: String, okMessage: String, okStyle: UIAlertAction.Style, okAction: @escaping () -> Void) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let addCategoryAction = UIAlertAction(title: okMessage, style: okStyle) { _ in
+        let okAlertAction = UIAlertAction(title: okMessage, style: okStyle) { _ in
             okAction()
         }
             
@@ -47,12 +51,18 @@ class AlertUtils {
             print("Cancel clicked")
         }
             
-        alert.addAction(addCategoryAction)
+        alert.addAction(okAlertAction)
         alert.addAction(cancelAction)
             
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScenes = scenes.first as? UIWindowScene
-            
-        windowScenes?.windows.first?.rootViewController?.present(alert, animated: true)
+        displayAlert(with: alert)
+    }
+    
+    static func displayNotifyingAlertView(title: String, message: String, okMessage: String, okStyle: UIAlertAction.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: okMessage, style: okStyle)
+        alert.addAction(okAction)
+        
+        displayAlert(with: alert)
     }
 }
