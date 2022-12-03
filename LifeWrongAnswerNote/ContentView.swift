170e0fb0367cc09b777fe675a938bb0933a2ee57
namgeon1106 <namgeon1106@naver.com>
2022-12-03 20:11:40 +0900

    Initial Commit

---
+++ LifeWrongAnswerNote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifeWrongAnswerNote/LifeWrongAnswerNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeWrongAnswerNoteApp.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct LifeWrongAnswerNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
