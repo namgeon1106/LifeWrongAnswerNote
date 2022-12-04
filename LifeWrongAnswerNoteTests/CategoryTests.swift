036c4d414dfc873a087ebeaf0309d0f7772f5e85
namgeon1106 <namgeon1106@naver.com>
2022-12-04 15:06:20 +0900

    test: Category extension 추가 및 by(name:) 메서드에 대한 테스트 코드 작성

---
+++ LifeWrongAnswerNoteTests/CategoryTests.swift
@@ -0,0 +1,48 @@
+//
+//  LifeWrongAnswerNoteTests.swift
+//  LifeWrongAnswerNoteTests
+//
+//  Created by 김남건 on 2022/12/04.
+//
+
+import XCTest
+import CoreData
+@testable import LifeWrongAnswerNote
+
+final class CategoryTests: XCTestCase {
+    var viewContext: NSManagedObjectContext!
+    
+    override class func setUp() {
+        CoreDataManager.shared.load(forTest: true)
+    }
+
+    override func setUpWithError() throws {
+        viewContext = CoreDataManager.shared.viewContext
+    }
+
+    override func tearDownWithError() throws {
+        let allCagetories = try! viewContext.fetch(Category.fetchRequest())
+        
+        
+        for cagetory in allCagetories {
+            try! cagetory.delete()
+        }
+    }
+    
+    func testByName_whenMatchingCategoryExists_returnsAnInstance() {
+        // given
+        let category1 = Category(context: viewContext)
+        category1.name = "category1"
+        
+        let category2 = Category(context: viewContext)
+        category2.name = "category2"
+        
+        try! viewContext.save()
+        
+        // when
+        let sut = try! Category.by(name: "category1")
+        
+        // then
+        XCTAssertEqual(sut?.objectID, category1.objectID)
+    }
+}
---
+++ Models/Category+.swift
@@ -0,0 +1,14 @@
+//
+//  Category+.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/12/04.
+//
+
+import Foundation
+
+extension Category {
+    static func by(name: String) throws -> Category? {
+        return nil
+    }
+}
