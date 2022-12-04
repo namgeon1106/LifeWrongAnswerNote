301b0ec2cc3d9438f7510de6d4ef749bc1a4fdc3
namgeon1106 <namgeon1106@naver.com>
2022-12-04 18:36:51 +0900

    feat: Problem.by에서 2번째 테스트 통과 위한 코드 구현

--- Models/Problem+.swift
+++ Models/Problem+.swift
@@ -10,6 +10,19 @@ import Foundation
 extension Problem {
     static func by(category: Category?, isFinished: Bool?, searchText: String) throws -> [Problem] {
         let request = Problem.fetchRequest()
+        var predicates = [NSPredicate]()
+        
+        if let category {
+            let categoryPredicate = NSPredicate(format: "%K = %@", #keyPath(Problem.category), category.objectID)
+            predicates.append(categoryPredicate)
+        }
+        
+        if let isFinished {
+            let finishedPredicate = NSPredicate(format: "%K = %i", #keyPath(Problem.finished), isFinished)
+            predicates.append(finishedPredicate)
+        }
+        
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
         
         return try viewContext.fetch(request)
     }
