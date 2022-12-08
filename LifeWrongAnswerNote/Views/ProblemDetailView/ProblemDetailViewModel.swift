bec1649e5d3046bbc63d1bba77f2c4742d4778f5
namgeon1106 <namgeon1106@naver.com>
2022-12-08 17:14:53 +0900

    feat: ProblemDetailViewModel안에서 problem의 choice 정보 받아오도록 구현

--- LifeWrongAnswerNote/Views/ProblemDetailView/ProblemDetailViewModel.swift
+++ LifeWrongAnswerNote/Views/ProblemDetailView/ProblemDetailViewModel.swift
@@ -16,16 +16,31 @@ class ProblemDetailViewModel: ObservableObject {
     @Published var assessment = Assessment.notSure
     
     @Published var situation = ""
-    @Published var choices = [TempChoice]()
+    @Published var tempChoices = [TempChoice]()
     @Published var reason = ""
     @Published var result = ""
     @Published var lesson = ""
+    
+    @Published var errorAlertIsPresented = false
+    @Published var errorMessage = "" {
+        didSet {
+            errorAlertIsPresented = true
+        }
+    }
     
     init(problemVM: ProblemViewModel?) {
         problem = problemVM?.problem ?? Problem(context: CoreDataManager.shared.viewContext)
         
         if problemVM == nil {
             problem.createdDate = .now
+        }
+        
+        do {
+            tempChoices = try Choice.by(problem: problem).map { choice in
+                return TempChoice(content: choice.content ?? "", isSelected: choice.isSelected)
+            }
+        } catch {
+            errorMessage = "문제의 세부 정보를 불러오는데 실패했습니다.\n화면을 나갔다가 다시 들어오세요."
         }
     }
 }
