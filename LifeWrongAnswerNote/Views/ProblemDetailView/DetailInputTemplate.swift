cc4d0fe8e037ace9f64ba07137a35bc1b25ec851
namgeon1106 <namgeon1106@naver.com>
2022-12-08 13:57:06 +0900

    feat: DetailInputTemplate 추가

---
+++ LifeWrongAnswerNote/Views/ProblemDetailView/DetailInputTemplate.swift
@@ -0,0 +1,37 @@
+//
+//  DetailInputTemplate.swift
+//  LifeWrongAnswerNote
+//
+//  Created by 김남건 on 2022/12/08.
+//
+
+import SwiftUI
+
+struct DetailInputTemplate<Content: View>: View {
+    let title: String
+    var content: () -> Content
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .padding(.leading, 11)
+                Spacer()
+            }
+            .padding(.bottom, 4)
+            Divider()
+            content()
+                .padding(.top, 16)
+                .padding(.horizontal, 11)
+            Spacer()
+        }
+    }
+}
+
+struct DetailInputTemplate_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailInputTemplate(title: "1. 어떤 상황인가요?") {
+            BorderedTextEditor(text: .constant(""), isEditable: false)
+        }
+    }
+}
